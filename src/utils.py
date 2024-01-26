2daa991b5a4fc903a551124a67acfc48bf5dbe26
ChallapalliHemanthsaikumar <67137724+ChallapalliHemanthsaikumar@users.noreply.github.com>
2024-01-26 01:51:28 -0500

    Hyperparameters tuning is done

--- src/utils.py
+++ src/utils.py
@@ -8,7 +8,8 @@ import pandas as pd
 from src.exception import CustomException
 from dill import dump
 # import pickle5 as pickle
-
+from sklearn.model_selection import GridSearchCV
+from src.logger import logging
 
 def save_object(file_path,obj):
     try:
@@ -20,12 +21,19 @@ def save_object(file_path,obj):
         raise CustomException(e,sys)
 
 
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train,y_train,X_test,y_test,models,params):
     try:
         report = {}
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            model.fit(X_train,y_train) 
+            para = params[list(models.keys())[i]]
+            
+            # logging.info(f"para {para} model {model} keys{list(models.keys())[i]}  values{list(models.keys())[i]} ")
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+            # model.fit(X_train,y_train) 
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
             train_model_score = r2_score(y_train,y_train_pred)
