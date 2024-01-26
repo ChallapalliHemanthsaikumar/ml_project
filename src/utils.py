23b7601e689b797317b08ca92ad443e31aa89e12
ChallapalliHemanthsaikumar <67137724+ChallapalliHemanthsaikumar@users.noreply.github.com>
2024-01-26 03:25:17 -0500

    localhost website created by

--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,7 @@ from sklearn.metrics import r2_score
 import numpy as np
 import pandas as pd
 from src.exception import CustomException
-from dill import dump
+from dill import dump,load
 # import pickle5 as pickle
 from sklearn.model_selection import GridSearchCV
 from src.logger import logging
@@ -43,3 +43,11 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,params):
     except Exception as e:
         raise CustomException(e,sys)
     
+
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return load(file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
+        
